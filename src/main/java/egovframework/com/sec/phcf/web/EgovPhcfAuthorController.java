3cfcff42d75fee3adb51565ab35791cd0b8e9ec1
ryangraekim <ryangraekim@hubizict.com>
2020-03-17 11:14:56 +0900

    permission

--- src/main/java/egovframework/com/sec/phcf/web/EgovPhcfAuthorController.java
+++ src/main/java/egovframework/com/sec/phcf/web/EgovPhcfAuthorController.java
@@ -17,6 +17,7 @@ import egovframework.com.sec.phcf.service.AuthManage;
 import egovframework.com.sec.phcf.service.AuthManageVO;
 import egovframework.com.sec.phcf.service.EgovPhcfAuthorService;
 import egovframework.com.sec.ram.service.EgovAuthorManageService;
+import egovframework.phcf.hubizCommonMethod.CommonMethod;
 import egovframework.phcf.util.JsonUtil;
 import egovframework.rte.fdl.property.EgovPropertyService;
 import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
@@ -36,7 +37,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
+import org.springframework.web.servlet.ModelAndView;
 import org.apache.log4j.Logger;
 
 /**
@@ -220,7 +221,7 @@ public class EgovPhcfAuthorController {
 	}
     
     @RequestMapping(value="/sec/phcf/EgovPhcfAuthorBanGoToUrl.do")
-	public String EgovPhcfAuthorBanGoToUrl(ModelMap model) throws Exception {
-        return "error/noAuthorRightErrorPage";
+	public ModelAndView EgovPhcfAuthorBanGoToUrl(ModelMap model) throws Exception {
+    	return CommonMethod.generalAlertThrowing("", "_content", "접근 권한이 없습니다!");
 	}
 }
